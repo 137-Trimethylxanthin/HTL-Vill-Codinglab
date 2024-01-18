33e04f78769a30f3f44db9ee44771042a0be5010
sitterM <sitterM@edu.htl-villach.at>
2024-01-18 14:08:59 +0100

    fast boi

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { page } from '$app/stores';
+
+    let errorMessage = 'Unknown error';
+    if ($page.error !== null) {
+        errorMessage = $page.error.message;
+    }
+</script>
+
+<h1>{$page.status}: {errorMessage}</h1>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script>
+
+    import "./style.css"
+    //TODO: temporär später in optionenen hab dafür schon a function geschreiben :)
+    document.documentElement.dataset.theme = "dark";
+
+    let icon = "";
+    let theme = "";
+    if (!localStorage.getItem('setTheme')){
+        localStorage.setItem('setTheme', 'light');
+    }
+
+    console.log(document.documentElement.dataset.theme);
+
+    if (document.documentElement.dataset.theme === "dark"){
+        icon = "sun";
+        theme = "dark";
+    } else {
+        icon = "moon";
+        theme = "light";
+    }
+
+    let isOpen = false;
+
+    function toggleHeader(){
+        isOpen = !isOpen;
+    }
+
+</script>
+
+<header>
+    {#if isOpen}
+        <div id="open">
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    {:else}
+        <div on:click={toggleHeader} id="close" ></div>
+    {/if}
+</header>
+
+<slot />
