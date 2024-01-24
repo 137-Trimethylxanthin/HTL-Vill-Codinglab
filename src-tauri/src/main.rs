89d171e337e5bbd2ce82774d070616b64fff876e
RECHBERGER Simon, 2AHIF <rechbers@edu.htl-villach.at>
2024-01-24 11:26:25 +0100

    added setup_user

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,29 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::process::Command;
+use tauri::api::path::document_dir;
+use tauri::Runtime;
+use std::time::SystemTime;
+use std::fs;
 
+fn get_sys_time_in_secs() -> u64 {
+    match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+        Ok(n) => n.as_secs(),
+        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    }
+}
+
+#[tauri::command]
+async fn setup_user<R: Runtime>(app: tauri::AppHandle<R>, window: tauri::Window<R>, name: &str) -> Result<(), bool> {
+    let filename = format!("{}_{}", name, get_sys_time_in_secs());
+    let document_directory = document_dir().unwrap().push(filename);
+    // println!("{}", document_directory.display());
+    let resource_path = app.path_resolver()
+        .resolve_resource("python/")
+        .expect("failed to resolve resource");
+    // fs::copy(resource_path, document_directory);
+    Ok(())
+}
 
 #[tauri::command]
 fn open_code_with_filename(handle: tauri::AppHandle, file_name: &str) {
@@ -29,7 +51,10 @@ fn open_code_with_filename(handle: tauri::AppHandle, file_name: &str) {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![open_code_with_filename])
+        .invoke_handler(tauri::generate_handler![
+            setup_user,
+            open_code_with_filename
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,31 @@
 <script lang="ts">
     import { invoke } from '@tauri-apps/api';
-    
-
 
     function openCodeWithFilename(fileName: string) {
         return function() {
             invoke('open_code_with_filename', { fileName })
         }
     }
+
+    function setupUser() {
+        invoke('setup_user', { name: "ms" }).then((res) => {
+            console.log(res)
+        })
+    }
 </script>
 
 <div >
     <h1>Wilkommen Freunde</h1>
 
+<<<<<<< HEAD
     <button class="levelbtn complete" on:click={openCodeWithFilename("level1.py")}>Start Lvl 1</button>
     <button class="levelbtn amArbeiten" on:click={openCodeWithFilename("level2.py")}>Start Lvl 2</button>
     <button class="levelbtn" on:click={openCodeWithFilename("level3.py")}>Start Lvl 3</button>
 </div>
+=======
+<button on:click={openCodeWithFilename("level1.py")}>Start Lvl 1</button>
+<button on:click={openCodeWithFilename("level2.py")}>Start Lvl 2</button>
+<button on:click={openCodeWithFilename("level3.py")}>Start Lvl 3</button>
+
+<button on:click={setupUser}>setup user</button>
+>>>>>>> cffab3d (added setup_user)
