d694745d5d6ac4a3c5e621d31f6a0e78d7bba105
gamersi <sigamer805@gmail.com>
2024-01-31 20:04:46 +0100

    added indicator and time on home

--- TODO.md
+++ TODO.md
@@ -1,5 +1,4 @@
 # TODO Max
-- home/+page.svelte -> Status & Zeit
 - Level 1 Fertig machen
 - Level 1 input automatisch highlighten
 
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import { nameStore } from "../../utils/stores";
     import { goto } from "$app/navigation";
-    import { onMount } from "svelte";
     import { invoke } from "@tauri-apps/api";
+  import { writable, type Writable } from "svelte/store";
 
     function switchLevel(level: number) {
         return () => {
@@ -10,15 +10,13 @@
         }
     }
 
-    let levels = [];
+    let levels: Writable<any[]> = writable([]);
 
-    onMount(() => {
-        invoke("get_levels").then((res: any) => {
-            if (res) {
-                levels = res;
-            }
-        })
-    })
+    invoke("get_levels").then((res: any) => {
+        if (res) {
+            levels.set(res)
+        }
+    });
 </script>
 
 <div class="title">
@@ -35,7 +33,12 @@
 
 <div class="levelSelect">
     <div class="tooltip-container">
-        <button class="levelbtn" on:click={switchLevel(1)}>Level 1</button>
+        <button class="levelbtn" on:click={switchLevel(1)}>Level 1
+        {#if $levels[0] !== undefined && $levels[0][0] === true}
+            <span class="icon">&#10004;</span>
+            <span class="time">{String(Math.floor($levels[1][0] / 60)).padStart(2, '0')}:{String($levels[1][0] % 60).padStart(2, '0')}</span>
+        {/if}
+        </button>
         <div class="tooltip">
             Dieses Level ist für komplette Anfänger die noch nie programmiert haben. <br>
             Hier werden dir die Grundlagen beigebracht.
@@ -43,7 +46,12 @@
     </div>
 
     <div class="tooltip-container">
-        <button class="levelbtn" on:click={switchLevel(2)}>Level 2</button>
+        <button class="levelbtn" on:click={switchLevel(2)}>Level 2
+        {#if $levels[0] !== undefined && $levels[0][1] === true}
+            <span class="icon">&#10004;</span>
+            <span class="time">{String(Math.floor($levels[1][1] / 60)).padStart(2, '0')}:{String($levels[1][1] % 60).padStart(2, '0')}</span>
+        {/if}
+        </button>
         <div class="tooltip">
             Dieses Level ist für Leute die schon ein mal Programmiert haben. <br>
             Es gibt eine Erklärung zu den Funktionen die du brauchst.
@@ -51,11 +59,34 @@
     </div>
 
     <div class="tooltip-container">
-        <button class="levelbtn" on:click={switchLevel(3)} >Level 3</button>
+        <button class="levelbtn" on:click={switchLevel(3)} >Level 3
+        {#if $levels[0] !== undefined && $levels[0][2] === true}
+            <span class="icon">&#10004;</span>
+            <span class="time">{String(Math.floor($levels[1][2] / 60)).padStart(2, '0')}:{String($levels[1][2] % 60).padStart(2, '0')}</span>
+        {/if}
+        </button>
         <div class="tooltip">
             Ein Level für die Profis. <br>
             Hier solltest du bereits die Grundlagen von Python kennen (oder die vorherigen Level gemacht haben).
         </div>
     </div>
-
 </div>
+
+<style>
+    .time {
+        font-size: 2vh;
+        margin-left: 1vh;
+        color: var(--text);
+        background-color: var(--overlay0);
+        padding: 0.5vh;
+        border-radius: 0.5vh;
+    }
+
+    .icon {
+        font-size: 2vh;
+        position: absolute;
+        right: 2vw;
+        color: var(--green);
+        /* top: 0.5vh; */
+    }
+</style>
